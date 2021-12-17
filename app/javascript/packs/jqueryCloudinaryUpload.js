08b0231793a47adf7ce74a8f0c65a446dd77db74
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-12-17 10:05:10 +0900

    refactor: 画像の作成、編集ページのスタイルを変更

--- app/javascript/packs/jqueryCloudinaryUpload.js
+++ app/javascript/packs/jqueryCloudinaryUpload.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+  const currentImage = $("#current-image");
+  const preview = $("#cloudinary-fileupload-preview");
+  const fileuploadStatus = $("#cloudinary-fileupload-status");
+  const fileuploadProgress = $("#cloudinary-fileupload-progress");
+  const fileuploadMessage = $("#cloudinary-fileupload-message");
   $(".cloudinary-fileupload")
     .cloudinary_fileupload({
       acceptFileTypes: /(\.|\/)(jpe?g|png|webp)$/i,
@@ -8,40 +13,39 @@ $(document).ready(function () {
         if (data.files.error) alert(data.files[0].error);
       },
       start: function (e) {
-        $("#cloudinary-fileupload-status").removeClass("is-hidden");
-        $("#cloudinary-fileupload-message").text("Starting upload...");
+        fileuploadStatus.removeClass("is-hidden");
+        fileuploadMessage.text("Starting upload...");
       },
       progress: function (e, data) {
-        $("#cloudinary-fileupload-progress").val(Math.round((data.loaded * 100.0) / data.total));
-        $("#cloudinary-fileupload-message").text(`Uploading... ${Math.round((data.loaded * 100.0) / data.total)}%`);
+        fileuploadProgress.val(Math.round((data.loaded * 100.0) / data.total));
+        fileuploadMessage.text(`Uploading... ${Math.round((data.loaded * 100.0) / data.total)}%`);
       },
       fail: function (e, data) {
-        $("#cloudinary-fileupload-message").text("Upload failed");
+        fileuploadMessage.text("Upload failed");
       }
     })
     .off("cloudinarydone").on("cloudinarydone", function (e, data) {
-      $("#cloudinary-fileupload-status").addClass("is-hidden");
-      var preview = $("#cloudinary-fileupload-preview").html("");
+      fileuploadStatus.addClass("is-hidden");
+      currentImage.addClass("is-hidden");
+      var column = $(`<div class="column is-2 is-flex"></div>`).appendTo(preview);
       $.cloudinary.image(data.result.public_id, {
         format: data.result.format, width: 500, height: 500, crop: "fill"
-      }).appendTo(preview);
+      }).appendTo($("<figure>").appendTo(column));
 
       $("<a/>").
         addClass("delete_by_token delete is-medium").
         attr({ href: "#" }).
         data({ delete_token: data.result.delete_token }).
         html("&times;").
-        appendTo(preview).
+        appendTo(column).
         click(function (e) {
           e.preventDefault();
           $.cloudinary.delete_by_token($(this).data("delete_token")).done(function () {
-            $("#cloudinary-fileupload-preview").html("");
-            $("#info").html("");
+            column.remove();
+            currentImage.removeClass("is-hidden");
             $('input[name="image[cl_id]"]').remove();
           }).fail(function () {
-            $("#cloudinary-fileupload-status").removeClass("is-hidden");
-            $("#cloudinary-fileupload-progress").addClass("is-hidden");
-            $("#cloudinary-fileupload-message").text("Cannot delete image");
+            alert("Cannot delete image");
           });
         });
     });
