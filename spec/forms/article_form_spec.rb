203a10f632f3c400e46139ab31cea613c925171c
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-12-18 12:40:41 +0900

    test: article_formのsaveメソッドで作成される記事と画像の数のテストを追加

--- spec/forms/article_form_spec.rb
+++ spec/forms/article_form_spec.rb
@@ -3,25 +3,47 @@ require 'rails_helper'
 RSpec.describe ArticleForm, type: :model do
   describe '#save' do
     context 'without images' do
-      it 'returns true' do
+      let(:article_form) do
         attributes = attributes_for(:article)
-        article_form = described_class.new(attributes, article: Article.new)
+        described_class.new(attributes, article: Article.new)
+      end
+
+      it 'returns true' do
         expect(article_form.save).to be_truthy
+      end
+
+      it 'increases article count by 1' do
+        expect { article_form.save }.to change(Article, :count).by(1)
+      end
+
+      it 'does not increase image count' do
+        expect { article_form.save }.to change(Image, :count).by(0)
       end
     end
 
     context 'with an image' do
-      it 'returns true' do
+      let(:article_form) do
         example_image = Rails.root.join("spec/fixtures/files/example.#{%w[jpg jpeg png webp].sample}")
         cl_ids = { cl_ids: [Rack::Test::UploadedFile.new(example_image)] }
         attributes = attributes_for(:article).merge(cl_ids)
-        article_form = described_class.new(attributes, article: Article.new)
+        described_class.new(attributes, article: Article.new)
+      end
+
+      it 'returns true' do
         expect(article_form.save).to be_truthy
+      end
+
+      it 'increases article count by 1' do
+        expect { article_form.save }.to change(Article, :count).by(1)
+      end
+
+      it 'increases image count by 1' do
+        expect { article_form.save }.to change(Image, :count).by(1)
       end
     end
 
     context 'with 10 images' do
-      it 'returns true' do
+      let(:article_form) do
         images = []
         10.times do
           example_image = Rails.root.join("spec/fixtures/files/example.#{%w[jpg jpeg png webp].sample}")
@@ -29,13 +51,24 @@ RSpec.describe ArticleForm, type: :model do
         end
         cl_ids = { cl_ids: images }
         attributes = attributes_for(:article).merge(cl_ids)
-        article_form = described_class.new(attributes, article: Article.new)
+        described_class.new(attributes, article: Article.new)
+      end
+
+      it 'returns true' do
         expect(article_form.save).to be_truthy
+      end
+
+      it 'increases article count by 1' do
+        expect { article_form.save }.to change(Article, :count).by(1)
+      end
+
+      it 'increases image count by 10' do
+        expect { article_form.save }.to change(Image, :count).by(10)
       end
     end
 
     context 'with 11 images' do
-      it 'returns false' do
+      let(:article_form) do
         images = []
         11.times do
           example_image = Rails.root.join("spec/fixtures/files/example.#{%w[jpg jpeg png webp].sample}")
@@ -43,9 +76,24 @@ RSpec.describe ArticleForm, type: :model do
         end
         cl_ids = { cl_ids: images }
         attributes = attributes_for(:article).merge(cl_ids)
-        article_form = described_class.new(attributes, article: Article.new)
+        described_class.new(attributes, article: Article.new)
+      end
+
+      it 'returns false' do
         expect(article_form.save).to be_falsey
+      end
+
+      it 'has the error' do
+        article_form.save
         expect(article_form.errors).to be_of_kind(:base, :too_many_images)
+      end
+
+      it 'does not increase article count' do
+        expect { article_form.save }.to change(Article, :count).by(0)
+      end
+
+      it 'does not increase image count' do
+        expect { article_form.save }.to change(Image, :count).by(0)
       end
     end
   end
